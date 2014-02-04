f865b8fa5a52e80f41b446887d22103424e8691b
DragonBS <milkotodorov@hotmail.com>
2014-02-05 01:03:33 +0200

    Diagram of basic char properties and classes

--- AwesomeRPGgameUsingOOP/AwesomeRPGgameUsingOOP/AwesomeRPGgameUsingOOP/Object classes/Character.cs
+++ AwesomeRPGgameUsingOOP/AwesomeRPGgameUsingOOP/AwesomeRPGgameUsingOOP/Object classes/Character.cs
@@ -12,22 +12,22 @@ namespace AwesomeRPGgameUsingOOP.Object_classes
        public int Damage { get; set; }
        public bool IsAlive { get; set; }
 
-        //HERO
-       public int FreeInventory { get; set; }
-       public int HealthOfHero{ get; set; }
-       public int ArmourOfHero{ get; set; }
-       public int DamageOfHero { get; set; }
-       public List<Item> Items { get; set; } 
+       // //HERO
+       //public int FreeInventory { get; set; }
+       //public int HealthOfHero{ get; set; }
+       //public int ArmourOfHero{ get; set; }
+       //public int DamageOfHero { get; set; }
+       //public List<Item> Items { get; set; } 
 
 
-        //NPC
-        override const bool IsAlive = true;
-        public List<Item> Items { get; set; } 
+       // //NPC
+       // override const bool IsAlive = true;
+       // public List<Item> Items { get; set; } 
 
-        //ENEMY
-        public int HealthOfEnemy { get; set; }
-        public int ArmourOfEnemy { get; set; }
-        public int DamageOfEnemy { get; set; }
-        public List<Item> Items { get; set; } 
+       // //ENEMY
+       // public int HealthOfEnemy { get; set; }
+       // public int ArmourOfEnemy { get; set; }
+       // public int DamageOfEnemy { get; set; }
+       // public List<Item> Items { get; set; } 
     }
 }
